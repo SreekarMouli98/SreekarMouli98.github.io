bd7ffc618773c35f19c3e27d34b536a70700d8e6
Sreekar Mouli <sreekar.mouli1998@gmail.com>
2020-08-16 00:11:10 +0530

    Added Protfolio component

---
+++ src/Portfolio/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Portfolio(props) {
+    return (
+        <React.Fragment>
+            Hello World!
+        </React.Fragment>
+    )
+}
+
+export default Portfolio;

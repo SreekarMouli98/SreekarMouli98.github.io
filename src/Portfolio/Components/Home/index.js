b84a5f68d3aacf2e26f064f7656198195cbac1ab
Sreekar Mouli <sreekar.mouli1998@gmail.com>
2021-05-15 14:27:02 +0530

    Home component mobile view

--- src/Portfolio/Components/Home/index.js
+++ src/Portfolio/Components/Home/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Context from '../../Context';
 
-const HOME_CONTENT_WIDTH  = 580;
-const HOME_CONTENT_HEIGHT = 400;
-
-const useStyles = makeStyles({
-  homeContent: {
-    position: 'fixed',
-    width   : `${HOME_CONTENT_WIDTH}px`,
-    left    : `calc(50% - ${HOME_CONTENT_WIDTH / 2}px)`,
-    height  : `${HOME_CONTENT_HEIGHT}px`,
-    top     : `calc(50% - ${HOME_CONTENT_HEIGHT / 2}px)`,
-  },
-});
+const HOME_CONTENT_WIDTH  = 573;
+const HOME_CONTENT_HEIGHT = 200;
+const MAX_MOBILE_VIEW_WIDTH = 670;
 
 function Home() {
+  const context = useContext(Context);
+
+  const calcScale = () => {
+    let diff = (MAX_MOBILE_VIEW_WIDTH - context.windowSize.width) / (MAX_MOBILE_VIEW_WIDTH);
+    return 1 - diff;
+  };
+
+  const useStyles = makeStyles({
+    homeContent: {
+      position: 'fixed',
+      width   : `${HOME_CONTENT_WIDTH}px`,
+      left    : `calc(50% - ${HOME_CONTENT_WIDTH / 2}px)`,
+      height  : `${HOME_CONTENT_HEIGHT}px`,
+      top     : `calc(50% - ${HOME_CONTENT_HEIGHT / 2}px)`,
+      transform: `scale(${context.windowSize.width > MAX_MOBILE_VIEW_WIDTH ? 1 : calcScale()})`
+    },
+  });
+
   const classes = useStyles();
 
   return (

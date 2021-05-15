72a3005f441cdecf3c55312a68f67f4429f6e805
Sreekar Mouli <sreekar.mouli1998@gmail.com>
2021-05-15 18:34:22 +0530

    Contact component mobile view

--- src/Portfolio/Components/Contact/index.js
+++ src/Portfolio/Components/Contact/index.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { Container, Grid, Typography, Link } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { Container, Grid, Typography, Link, Toolbar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Context from '../../Context';
 
-const CONTENT_WIDTH  = 800;
-const CONTENT_HEIGHT = 400;
-
-const useStyles = makeStyles({
-  content: {
-    position: 'fixed',
-    width   : `${CONTENT_WIDTH}px`,
-    left    : `calc(50% - ${CONTENT_WIDTH / 2}px)`,
-    height  : `${CONTENT_HEIGHT}px`,
-    top     : `calc(50% - ${CONTENT_HEIGHT / 2}px)`,
-  },
-});
+const MAX_MOBILE_VIEW_WIDTH = 670;
+const REDUCE_PADDING_WIDTH  = 1000;
 
 function Contact() {
+  const context = useContext(Context);
+
+  const useStyles = makeStyles({
+    content: {
+      paddingLeft : context.windowSize.width > REDUCE_PADDING_WIDTH ? '200px' : '50px',
+      paddingRight: context.windowSize.width > REDUCE_PADDING_WIDTH ? '200px' : '50px',
+    },
+  });
+
   const classes = useStyles();
 
   return (
     <React.Fragment>
+      <Toolbar />
       <Container>
+        {context.windowSize.width <= MAX_MOBILE_VIEW_WIDTH && (
+          <React.Fragment>
+            <Typography variant='h3' align='center'>
+              CONTACT
+            </Typography>
+            <Toolbar />
+          </React.Fragment>
+        )}
         <div className={classes.content}>
           <Typography
             variant='h5'
@@ -29,27 +38,26 @@ function Contact() {
           >
             You can reach out to me over my email.
           </Typography>
-          <br />
-          <br />
-          <Grid
-            container
-            direction='column'
-            alignItems='center'
-            justify='space-evenly'
-          >
-            <Grid item>
-              <Typography variant='button'>
-                EMAIL
-              </Typography>
-              <hr />
-            </Grid>
-            <Grid item>
-              <Typography variant='button'>
-                <Link href='mailto:sreekar.mouli1998@gmail.com'>sreekar.mouli1998@gmail.com</Link>
-              </Typography>
-            </Grid>
-          </Grid>
         </div>
+        <Toolbar />
+        <Grid
+          container
+          direction='column'
+          alignItems='center'
+          justify='space-evenly'
+        >
+          <Grid item>
+            <Typography variant='button'>
+              EMAIL
+            </Typography>
+            <hr />
+          </Grid>
+          <Grid item>
+            <Typography variant='button'>
+              <Link href='mailto:sreekar.mouli1998@gmail.com'>sreekar.mouli1998@gmail.com</Link>
+            </Typography>
+          </Grid>
+        </Grid>
       </Container>
     </React.Fragment>
   );
